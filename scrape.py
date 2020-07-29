2b5f3abdf9f4b9e9c4b2a0fea8b65757e73c5c91
Chas Veres <Chas@Chass-MacBook-Pro.local>
2020-07-29 13:33:39 -0400

    Updated for two pages

--- scrape.py
+++ scrape.py
@@ -6,17 +6,25 @@ import pprint
 # BS4 will do the scraping
 
 res = requests.get('https://news.ycombinator.com/news')
-
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 # HTML mode specified (HTML <----> XML)
 
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+
 links = soup.select('.storylink')
+links2 = soup2.select('.storylink')
+
 subtext = soup.select('.subtext')
+subtext2 = soup2.select('.subtext')
+
+all_links = links + links2
+all_subtext = subtext + subtext2
 
 def sort_stories_by_votes(news_list):
     return sorted(news_list, key= lambda x:x['votes'], reverse=True)
 
-def create_custom_news(links, votes):
+def create_custom_news(links, subtext):
     news = []
     for index, item in enumerate(links):
         title = item.getText()
@@ -28,4 +36,4 @@ def create_custom_news(links, votes):
                 news.append({'title': title, 'link': href, 'votes': points})
     return sort_stories_by_votes(news)
 
-pprint.pprint(create_custom_news(links, subtext))
+pprint.pprint(create_custom_news(all_links, all_subtext))
